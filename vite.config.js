982dbda8741d582f055fc614248c7c0d167638b4
Darren <darren.xie@pkmgindo.com>
2025-10-24 07:59:45 +0700

    fixing vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',  // Change this from '/final_ass-main/' to './'
+  base: '/assignment01/',
   server: {
     port: 3000,
     hmr: {
